37c51b6e16b7832334a29748481a280e19afc57d
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2023-03-15 17:54:33 -0300

    render it separately to get frontmatter

---
+++ DataInputs/csasdown/render.R
@@ -0,0 +1,11 @@
+#Step 1: render the csasdown project
+csasdown::render()
+# Step 2: Add initial pages of a typical resdoc (title page, etc.)
+frontmatter <- officer::read_docx(path = "templates/RES2021-eng-frontmatter.docx")
+resdoc <- "_book/resdoc-english.docx"
+x <- officer::body_add_docx(frontmatter, resdoc, pos = "before")
+# Step 3: Add table of contents
+x <- officer::cursor_reach(x, keyword = "TABLE OF CONTENTS")
+x <- officer::body_add_toc(x)
+# Step 4: Save
+print(x, target = "_book/final_resdoc.docx")
