05142f7d3e4b62843ebe8a15e79fe21f532a5c47
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2023-05-03 14:22:13 -0300

    polishing ageing figures

--- DataInputs/csasdown/render.R
+++ DataInputs/csasdown/render.R
@@ -1,4 +1,5 @@
 #Step 1: render the csasdown project
+# (working directory must be set to Framework/DataInputs/csasdown)
 csasdown::render()
 # Step 2: Add initial pages of a typical resdoc (title page, etc.)
 frontmatter <- officer::read_docx(path = "templates/RES2021-eng-frontmatter.docx")
@@ -8,4 +9,5 @@ x <- officer::body_add_docx(frontmatter, resdoc, pos = "before")
 x <- officer::cursor_reach(x, keyword = "TABLE OF CONTENTS")
 x <- officer::body_add_toc(x)
 # Step 4: Save
+# If error message, look for temp file in RStudio Files panel. Select box, and delete in Rstudio. Temp file may not be visible in File explorer.
 print(x, target = "_book/final_resdoc.docx")
