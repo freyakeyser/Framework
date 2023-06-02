771f9f74388d8e0286d9fd35467fed0082f2065f
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2023-06-02 17:04:50 -0300

    for author meeting, plus some post-meeting edits

--- DataInputs/MWSH/growth_analysis.R
+++ DataInputs/MWSH/growth_analysis.R
@@ -13,9 +13,10 @@ require(ggplot2)
 require(tidyverse)
 require(gratia)
 
-
 # hydration data
 load("C:/Users/keyserf/Documents/temp_data/testing_results_framework_75-90.RData")
+
+plotsGo <- "Y:/Offshore/Assessment/Framework/SFA_25_26_2024/DataInputs/MWSH"
 
 bank <- "BBn"
 dat <- mw.dat.all[[bank]]
@@ -155,23 +156,23 @@ mod_eval <- function(mod){
 }
 
 
-gam_cf <- gam(data=dat,
-              cf ~ s(lon,lat) + s(depth) + as.factor(year),
-              family=Gamma(link="log"))
-gam_cf <- mod_eval(gam_cf)
+# gam_cf <- gam(data=dat,
+#               cf ~ s(lon,lat) + s(depth) + as.factor(year),
+#               family=Gamma(link="log"))
+# gam_cf <- mod_eval(gam_cf)
 
 
-glm_cf <- glm(data=dat,
-              cf ~ depth + as.factor(year),
-              family=Gamma(link="log"))
-glm_cf <- mod_eval(glm_cf)
+# glm_cf <- glm(data=dat,
+#               cf ~ depth + as.factor(year),
+#               family=Gamma(link="log"))
+# glm_cf <- mod_eval(glm_cf)
 
 
-gamm_cf <- gamm(data=dat,
-                cf ~ as.factor(year) + s(depth) + s(lon, lat),
-                random=list(ID=~1),
-                family=Gamma(link="log"))
-gamm_cf <- mod_eval(gamm_cf)
+# gamm_cf <- gamm(data=dat,
+#                 cf ~ as.factor(year) + s(depth) + s(lon, lat),
+#                 random=list(ID=~1),
+#                 family=Gamma(link="log"))
+# gamm_cf <- mod_eval(gamm_cf)
 
 # with lon lat it goes high
 
@@ -183,6 +184,15 @@ gamm_cf <- mod_eval(gamm_cf)
 # Need to skip MWSH and use condition.
 # OR you could estimate MW, and then predict on 100mm individual. Let's try that.
 # same model as above.
+
+# include tow as random effect in all models (glmm/gamm)
+# centre wmw and sh
+# copy inshore mwsh model
+# run across all years and run for individual years (individual years will need depth:year)
+# include depth and not
+# determine gaussian and gamma using residuals. Start with gaussian, and only do gamma if necessary (because resids suck)
+# see yihao yin 2022 spa3 paper canjfish (can cite this for gaussian)
+# https://github.com/Mar-scal/Inshore/blob/main/SFA29/Growth/SFA29_MeatWeightShellHeight.R
 
 glm_mw <- glm(data=dat, wmw ~ sh + as.factor(year), family=gaussian(link="log"))
 glm_mw <- mod_eval(glm_mw)
@@ -200,20 +210,20 @@ gam_mw <- mod_eval(gam_mw)
 
 # pretty darn good
 
-gam_mw2 <- gam(data=dat, wmw ~ sh + s(lon, lat) + s(depth) + as.factor(year))
-gam_mw2 <- mod_eval(gam_mw2)
+# gam_mw2 <- gam(data=dat, wmw ~ sh + s(lon, lat) + s(depth) + as.factor(year))
+# gam_mw2 <- mod_eval(gam_mw2)
 # gam for MW with gamma log link wins so far
 
 # what about log transforming sh
 
-gam_mw3 <- gam(data=dat, wmw ~ log(sh) + s(lon, lat) + s(depth) + as.factor(year))
-gam_mw3 <- mod_eval(gam_mw3)
+# gam_mw3 <- gam(data=dat, wmw ~ log(sh) + s(lon, lat) + s(depth) + as.factor(year))
+# gam_mw3 <- mod_eval(gam_mw3)
 #super fast
 
 # what about sh^3 maybe?
 
-gam_mw4 <- gam(data=dat, wmw ~ sh^3 + s(lon, lat) + s(depth) + as.factor(year))
-gam_mw4 <- mod_eval(gam_mw4)
+# gam_mw4 <- gam(data=dat, wmw ~ sh^3 + s(lon, lat) + s(depth) + as.factor(year))
+# gam_mw4 <- mod_eval(gam_mw4)
 #super fast
 
 # what about sh^3 maybe? and log link?
@@ -230,24 +240,32 @@ glm_mw6 <- mod_eval(glm_mw6)
 # pretty close to original
 
 # gam_cf, glm_cf, gamm_cf, glm_mw, glm_mw_gamma, gam_mw, gam_mw2, gam_mw3, gam_mw4, gam_mw5, glm_mw6
-cols <- viridis::viridis(n=13)
-mods <- c(gam_cf, glm_cf, gamm_cf, glm_mw, glm_mw_gamma, gam_mw, gam_mw2, gam_mw3, gam_mw4, gam_mw5, glm_mw6)
+# cols <- viridis::viridis(n=13)
+# mods <- c(gam_cf, glm_cf, gamm_cf, glm_mw, glm_mw_gamma, gam_mw, gam_mw2, gam_mw3, gam_mw4, gam_mw5, glm_mw6)
+#
+# colors <- c("gam_cf" = cols[1],
+#             "glm_cf" = cols[2],
+#             "gamm_cf" = cols[3],
+#             "glm_mw" = cols[4],
+#             "glm_mw_gamma" = cols[5],
+#             "gam_mw" = cols[6],
+#             "gam_mw2" = cols[7],
+#             "gam_mw3" = cols[8],
+#             "gam_mw4" = cols[9],
+#             "gam_mw5" = cols[10],
+#             "glm_mw6" = cols[11],
+#             "orig" = cols[12],
+#             "orig2" = cols[13])
 
-colors <- c("gam_cf" = cols[1],
-            "glm_cf" = cols[2],
-            "gamm_cf" = cols[3],
-            "glm_mw" = cols[4],
-            "glm_mw_gamma" = cols[5],
-            "gam_mw" = cols[6],
-            "gam_mw2" = cols[7],
-            "gam_mw3" = cols[8],
-            "gam_mw4" = cols[9],
-            "gam_mw5" = cols[10],
-            "glm_mw6" = cols[11],
-            "orig" = cols[12],
-            "orig2" = cols[13])
+cols <- viridis::viridis(n=6)
+colors <- c("glm_mw" = cols[1],
+            "glm_mw_gamma" = cols[2],
+            "gam_mw5" = cols[3],
+            "glm_mw6" = cols[4],
+            "orig" = cols[5],
+            "orig2" = cols[6])
 
-
+png(paste0(plotsGo, "/", bank, "/CF_model_compare.png"), height=4, width=6.5, units="in", res=420)
 ggplot() + geom_line(data=condfac_res$CFyrs, aes(as.numeric(year), CF, colour="orig"),group=1) +
   geom_line(data=CFyrs, aes(year, fit, colour="orig2"), group=1) +
   #geom_line(data=gam_cf$preds, aes(as.numeric(year), inv_link(gam_cf$mod)(fit), colour="gam_cf"), group=1) +
@@ -261,8 +279,10 @@ ggplot() + geom_line(data=condfac_res$CFyrs, aes(as.numeric(year), CF, colour="o
   #geom_line(data=gam_mw4$preds, aes(as.numeric(year), inv_link(gam_mw4$mod)(fit), colour="gam_mw4"), group=1) +
   geom_line(data=gam_mw5$preds, aes(as.numeric(year), inv_link(gam_mw5$mod)(fit), colour="gam_mw5"), group=1) +
   geom_line(data=glm_mw6$preds, aes(as.numeric(year), inv_link(glm_mw6$mod)(fit), colour="glm_mw6"), group=1) +
-  scale_color_manual(values = colors)
-
+  scale_color_manual(values = colors, name="Model name") +
+  theme_bw() +
+  xlab("Year")
+dev.off()
 # best ones so far
 
 
@@ -305,75 +325,75 @@ mesh$mesh$n # number of vertices or knots
 
 ### Fitting spatial models ########################################################################################
 
-fit_spatial_gam <- sdmTMB(
-  wmw ~ s(sh), # intercept only
-  data = dat,
-  family = lognormal(),#tweedie(link = "log"),
-  mesh = mesh,
-  spatial = "on",
-  control = sdmTMBcontrol(newton_loops = 1)#
-  # silent = FALSE
-)
-
-# check fit
-sanity(fit_spatial_gam)
-fit_spatial_gam
-
-#residuals
-resid <- residuals(fit_spatial_gam)
-qqnorm(resid[is.finite(resid)])
-qqline(resid[is.finite(resid)])
-dat$resid <- residuals(fit_spatial_gam)
-ggplot() +
-  geom_point(data=dat, aes(X, Y, fill=resid))+
-  facet_wrap(~year)
-
-ggplot() + geom_point(data=dat[is.finite(dat$resid),], aes(sh, resid))
-# not very starry
-
-#calculate residuals manually
-resid <- predict(fit_spatial_gam, dat) # where dat was the dataframe originally used for fitting model
-resid$resid.log <- resid$wmw - resid$est
-resid$resid.resp <- resid$wmw - exp(resid$est)
-ggplot() + geom_point(data=resid, aes(sh, resid.log))
-ggplot() + geom_point(data=resid, aes(sh, resid.resp))
-# not very starry
-
-# log instead?
-fit_spatial_log <- sdmTMB(
-  wmw ~ log(sh),
-  data = dat,
-  family = lognormal(),#tweedie(link = "log"),
-  mesh = mesh,
-  spatial = "on"#,
-  #control = sdmTMBcontrol(newton_loops = 1)#
-  # silent = FALSE
-)
-
-# check fit
-sanity(fit_spatial_log)
-fit_spatial_log
-
-#residuals
-resid <- residuals(fit_spatial_log)
-qqnorm(resid[is.finite(resid)])
-qqline(resid[is.finite(resid)])
-dat$resid <- residuals(fit_spatial_log)
-ggplot() +
-  geom_point(data=dat, aes(X, Y, fill=resid))+
-  facet_wrap(~year)
-
-ggplot() + geom_point(data=dat[is.finite(dat$resid),], aes(sh, resid))
-# not very starry
-
-#calculate residuals manually
-resid <- predict(fit_spatial_log, dat) # where dat was the dataframe originally used for fitting model
-resid$resid.log <- resid$wmw - resid$est
-resid$resid.resp <- resid$wmw - exp(resid$est)
-ggplot() + geom_point(data=resid, aes(sh, resid.log))
-ggplot() + geom_point(data=resid, aes(sh, resid.resp))
-# not very starry
-
+# fit_spatial_gam <- sdmTMB(
+#   wmw ~ s(sh), # intercept only
+#   data = dat,
+#   family = lognormal(),#tweedie(link = "log"),
+#   mesh = mesh,
+#   spatial = "on",
+#   control = sdmTMBcontrol(newton_loops = 1)#
+#   # silent = FALSE
+# )
+#
+# # check fit
+# sanity(fit_spatial_gam)
+# fit_spatial_gam
+#
+# #residuals
+# resid <- residuals(fit_spatial_gam)
+# qqnorm(resid[is.finite(resid)])
+# qqline(resid[is.finite(resid)])
+# dat$resid <- residuals(fit_spatial_gam)
+# ggplot() +
+#   geom_point(data=dat, aes(X, Y, fill=resid))+
+#   facet_wrap(~year)
+#
+# ggplot() + geom_point(data=dat[is.finite(dat$resid),], aes(sh, resid))
+# # not very starry for BBn, better for Sab
+#
+# #calculate residuals manually
+# resid <- predict(fit_spatial_gam, dat) # where dat was the dataframe originally used for fitting model
+# resid$resid.log <- resid$wmw - resid$est
+# resid$resid.resp <- resid$wmw - exp(resid$est)
+# ggplot() + geom_point(data=resid, aes(sh, resid.log))
+# ggplot() + geom_point(data=resid, aes(sh, resid.resp))
+# # not very starry for BBn or Sab
+#
+# # log instead?
+# fit_spatial_log <- sdmTMB(
+#   wmw ~ log(sh),
+#   data = dat,
+#   family = lognormal(),#tweedie(link = "log"),
+#   mesh = mesh,
+#   spatial = "on"#,
+#   #control = sdmTMBcontrol(newton_loops = 1)#
+#   # silent = FALSE
+# )
+#
+# # check fit
+# sanity(fit_spatial_log)
+# fit_spatial_log
+#
+# #residuals
+# resid <- residuals(fit_spatial_log)
+# qqnorm(resid[is.finite(resid)])
+# qqline(resid[is.finite(resid)])
+# dat$resid <- residuals(fit_spatial_log)
+# ggplot() +
+#   geom_point(data=dat, aes(X, Y, fill=resid))+
+#   facet_wrap(~year)
+#
+# ggplot() + geom_point(data=dat[is.finite(dat$resid),], aes(sh, resid))
+# # not very starry for BBn, better for Sab
+#
+# #calculate residuals manually
+# resid <- predict(fit_spatial_log, dat) # where dat was the dataframe originally used for fitting model
+# resid$resid.log <- resid$wmw - resid$est
+# resid$resid.resp <- resid$wmw - exp(resid$est)
+# ggplot() + geom_point(data=resid, aes(sh, resid.log))
+# ggplot() + geom_point(data=resid, aes(sh, resid.resp))
+# # not very starry for either
+#
 
 ### Fitting spatiotemporal model ########################################################################################
 
@@ -396,8 +416,10 @@ fit_spatial_ssh_st
 
 #residuals
 resid <- residuals(fit_spatial_ssh_st)
+png(paste0(plotsGo, "/", bank, "/sdmtmb_qqplot.png"), height=4, width=6.5, res=420, units="in")
 qqnorm(resid[is.finite(resid)])
 qqline(resid[is.finite(resid)])
+dev.off()
 dat$resid <- residuals(fit_spatial_ssh_st)
 ggplot() +
   geom_point(data=dat, aes(X, Y, fill=resid))+
@@ -410,7 +432,9 @@ resid <- predict(fit_spatial_ssh_st, dat) # where dat was the dataframe original
 resid$resid.log <- log(resid$wmw) - resid$est
 resid$resid.resp <- resid$wmw - exp(resid$est)
 
-ggplot() + geom_point(data=resid, aes(sh, resid.log))
+png(paste0(plotsGo, "/", bank, "/sdmtmb_resid.png"), height=4, width=6.5, res=420, units="in")
+ggplot() + geom_point(data=resid, aes(sh, resid.log)) +theme_bw()
+dev.off()
 ggplot() + geom_point(data=resid, aes(sh, resid.resp))
 
 # that looks a bit better... still not great though. Use simulations to evaluate later.
@@ -419,7 +443,7 @@ ggplot() + geom_point(data=resid, aes(sh, resid.resp))
 source("C:/Users/keyserf/Documents/Github/Assessment_fns/Maps/github_spatial_import.R")
 survey <- github_spatial_import(subfolder = "survey_boundaries", zipname = "survey_boundaries.zip")
 # convert it to the CRS used for the model
-survey2 <- survey[survey$ID=="BBn",] %>%
+survey2 <- survey[survey$ID==bank,] %>%
   st_transform(32619)
 
 # rasterize it
@@ -445,17 +469,28 @@ p$Y <- p$Y*1000
 simple.grid <- predict.grid[predict.grid$X == sort(predict.grid$X)[round(length(predict.grid$X)/2)] &
                               predict.grid$Y == sort(predict.grid$Y)[round(length(predict.grid$Y)/2)],]
 overall <- predict(fit_spatial_ssh_st, newdata=simple.grid)
+
+png(paste0(plotsGo, "/", bank, "/sdmtmb_mwsh.png"), height=4, width=6.5, res=420, units="in")
 ggplot() +
-  geom_point(data=dat, aes(sh, wmw), alpha=0.1) +
-  geom_line(data=overall, aes(sh, exp(est), group=year), colour="red")
+  geom_point(data=dat, aes(sh*100, wmw), alpha=0.1) +
+  geom_line(data=overall, aes(sh*100, exp(est), group=year), colour="red") +
+  theme_bw() +
+  xlab("Shell height (mm)") +
+  ylab("Meat weight (g)")
+dev.off()
 
 # plot as time series (wmw where sh=100mm)
 overall <-  predict(fit_spatial_ssh_st, newdata=simple.grid[simple.grid$sh==1,], se_fit=T)
+png(paste0(plotsGo, "/", bank, "/sdmtmb_ts.png"), height=4, width=6.5, res=420, units="in")
 ggplot() + geom_line(data=overall, aes(year, exp(est), group=1)) +
   geom_point(data=overall, aes(year, exp(est))) +
   geom_errorbar(data=overall, aes(year,
                                   ymin=exp(est-1.96*est_se),
-                                  ymax=exp(est+1.96*est_se)), width=0)
+                                  ymax=exp(est+1.96*est_se)), width=0) +
+  ylab("Condition factor") +
+  xlab("Year") +
+  theme_bw()
+dev.off()
 
 
 # crop out the raster cells outside the survey domain
@@ -511,11 +546,42 @@ ggplot() +
   scale_fill_viridis_c(trans="log10")
 
 # piece de resistance:
+forlims <- st_transform(spdp, 4326)
+if(bank=="BBn") xdiv <- 0.3; ydiv <- 0.1; wide = 6.5*1.5; high = 5.5*1.5
+if(bank=="Sab") xdiv <- 0.6; ydiv <- 0.2; wide = 6.5*1.5; high = 6*1.5
+xbreaks <- seq(floor(st_bbox(forlims)$xmin[[1]]/2)*2, ceiling(st_bbox(forlims)$xmax[[1]]/2)*2, xdiv)
+ybreaks <- seq(floor(st_bbox(forlims)$ymin[[1]]/2)*2, ceiling(st_bbox(forlims)$ymax[[1]]/2)*2, ydiv)
+png(paste0(plotsGo, "/", bank, "/sdmtmb_cf.png"), width=wide, height=high, units="in", res=420)
 ggplot() +
   geom_sf(data=spdp, aes(fill = bin), colour=NA)+
   facet_wrap(~year) +
   # scale_fill_gradient2()#+
-  scale_fill_viridis_d()
+  scale_fill_viridis_d(name="Condition factor") +
+  scale_x_continuous(breaks=xbreaks) +
+  scale_y_continuous(breaks=ybreaks) +
+  theme_bw()
+dev.off()
 
 
+#### comparing all models:
+colors <- c(colors, sdmtmb="red")
 
+png(paste0(plotsGo, "/", bank, "/CF_model_compare2.png"), height=4, width=6.5, units="in", res=420)
+ggplot() + geom_line(data=condfac_res$CFyrs, aes(as.numeric(year), CF, colour="orig"),group=1) +
+  geom_line(data=CFyrs, aes(year, fit, colour="orig2"), group=1) +
+  #geom_line(data=gam_cf$preds, aes(as.numeric(year), inv_link(gam_cf$mod)(fit), colour="gam_cf"), group=1) +
+  #geom_line(data=glm_cf$preds, aes(as.numeric(year), inv_link(glm_cf$mod)(fit), colour="glm_cf"), group=1) +
+  #geom_line(data=gamm_cf$preds, aes(as.numeric(year), inv_link(gamm_cf$mod)(fit), colour="gamm_cf"), group=1) +
+  geom_line(data=glm_mw$preds, aes(as.numeric(year), inv_link(glm_mw$mod)(fit), colour="glm_mw"), group=1) +
+  geom_line(data=glm_mw_gamma$preds, aes(as.numeric(year), inv_link(glm_mw_gamma$mod)(fit), colour="glm_mw_gamma"), group=1) +
+  #geom_line(data=gam_mw$preds, aes(as.numeric(year), inv_link(gam_mw$mod)(fit), colour="gam_mw"), group=1) +
+  #geom_line(data=gam_mw2$preds, aes(as.numeric(year), inv_link(gam_mw2$mod)(fit), colour="gam_mw2"), group=1) +
+  #geom_line(data=gam_mw3$preds, aes(as.numeric(year), inv_link(gam_mw3$mod)(fit), colour="gam_mw3"), group=1) +
+  #geom_line(data=gam_mw4$preds, aes(as.numeric(year), inv_link(gam_mw4$mod)(fit), colour="gam_mw4"), group=1) +
+  geom_line(data=gam_mw5$preds, aes(as.numeric(year), inv_link(gam_mw5$mod)(fit), colour="gam_mw5"), group=1) +
+  geom_line(data=glm_mw6$preds, aes(as.numeric(year), inv_link(glm_mw6$mod)(fit), colour="glm_mw6"), group=1) +
+  geom_line(data=overall, aes(as.numeric(year), exp(est), colour="sdmtmb"), group=1)+
+  scale_color_manual(values = colors, name="Model name") +
+  theme_bw() +
+  xlab("Year")
+dev.off()
