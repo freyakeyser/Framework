8a59583d9071e53db6c358f20d61d7eae2c2a820
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2024-02-13 18:52:55 -0400

    adding glm option for low tow years

--- DataInputs/MWSH/DK_growth_analysis.R
+++ DataInputs/MWSH/DK_growth_analysis.R
@@ -367,6 +367,7 @@ yellows <- "#FFDD00"
 mod.res <- NULL
 resids <- NULL
 qq.plt <- NULL
+warningsdf <- NULL
 for(i in 1:n.yrs)
 {
   print(yrs[i])
@@ -376,12 +377,30 @@ for(i in 1:n.yrs)
   #if(yrs[i] >= 2011) mod.res[[as.character(yrs[i])]] <- glmer(wmw ~ log.sh.cen + poly(depth.cen,3) + (1| new_ID),data = dat.tmp,family=Gamma(link=log))
   # No non-linearity if we don't have 10 tows to use, to complex...
   # ONLY RUN RANDOM EFFECTS IF AT LEAST 5 TOWS
-  if(n.tows>4)  mod.res[[as.character(yrs[i])]] <- glmer(wmw ~ log.sh.cen + depth.cen + (1| new_ID),data = dat.tmp,family=Gamma(link=log))
-  if(n.tows<5)  mod.res[[as.character(yrs[i])]] <- glm(wmw ~ log.sh.cen + depth.cen,data = dat.tmp,family=Gamma(link=log))
+  if(n.tows>4)  {
+    mod.res[[as.character(yrs[i])]] <- glmer(wmw ~ log.sh.cen + depth.cen + (1| new_ID),data = dat.tmp,family=Gamma(link=log))
+
+    if(any(grepl("failed to converge",  mod.res[[as.character(yrs[i])]]@optinfo$conv$lme4$messages))|
+       any(grepl("unidentifiable",  mod.res[[as.character(yrs[i])]]@optinfo$conv$lme4$messages))){
+      warning <- data.frame(year = yrs[i], warn = mod.res[[as.character(yrs[i])]]@optinfo$conv$lme4$messages)
+      warningsdf <- rbind(warningsdf, warning)
+    }
+  }
+  if(n.tows<5)  {
+    mod.res[[as.character(yrs[i])]] <- glm(wmw ~ log.sh.cen + depth.cen,data = dat.tmp,family=Gamma(link=log))
+
+    if(mod.res[[as.character(yrs[i])]]$converged==F){
+      warning <- data.frame(year = yrs[i], warn = "check_glm")
+      warningsdf <- rbind(warningsdf, warning)
+    }
+  }
   dat.tmp$residuals <- residuals(mod.res[[as.character(yrs[i])]])
   resids[[as.character(yrs[i])]] <- dat.tmp
   #qq.plt[[as.character(yrs[i])]] <- qqplot.data(dat.tmp$residuals)
 }
+# check out warningsdf for convergence issues
+warningsdf
+
 # 1991 doesn't converge and we don't need it for the models, so I suggest we only use 1992 onwards for this.
 resid <- do.call('rbind',resids)
 
